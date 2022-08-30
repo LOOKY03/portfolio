3ddbf2dc0a95eec3041dd9ee1b1208f1512a8dd2
LOOKY03 <vajearvin26@gmail.com>
2022-08-31 01:07:17 +0800

    added contact form

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    maxWidth: {
-      'max-w-screen-1350': '1350px',
-      'max-w-screen-1300': '1300px',
-    },
     extend: {
       height: {
-        '80vh': '80vh',
-        '60vh': '60vh',
-        '100vh': '100vh',
+        "80vh": "80vh",
+        "60vh": "60vh",
+        "100vh": "100vh",
       },
       colors: {
-        'bg-aboutme': '#FCF6F0',
+        "bg-aboutme": "#FCF6F0",
       },
       screens: {
-        '2xl': '2560px',
-      }
+        "2xl": "2560px",
+      },
+      maxWidth: {
+        "max-w-screen-1350": "1350px",
+        "max-w-screen-1300": "1300px",
+      },
     },
   },
   plugins: [],
-}
+};
