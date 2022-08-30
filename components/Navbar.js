db9f83911ff9cb3ce49388bbe8df4f484ec324d4
LOOKY03 <vajearvin26@gmail.com>
2022-08-31 00:23:54 +0800

    added sticky navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,14 +3,17 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="max-w-screen-2xl mx-auto text-white">
+    <div className="max-w-screen-2xl mx-auto text-white fixed top-0 z-10">
       <div className="flex justify-between items-center pl-10 pr-10 py-2">
+        <Link href='/'>
         <img
-          className="h-16 cursor-pointer"
+          className="h-16 cursor-pointer fixed top-5 left-0 pl-10"
           src="/arvin-logo-png-transparent.png"
           alt=""
         />
-        <div className="flex space-x-5 ">
+        </Link>
+       
+        <div className="flex space-x-5 fixed right-10 top-5">
           <Link href="#about">
           <h1 className="cursor-pointer p-2 px-3 text-xl rounded-md hover:bg-amber-800">
             About
